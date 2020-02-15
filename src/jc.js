9a5c27c02d3656b0f6da01ab9a5fa769efcdb042
Marcos Henrique Stuber Dreveck <marcoshsdreveck@gmail.com>
2020-02-15 17:04:17 -0300

    envio do arquivo

--- src/jc.js
+++ src/jc.js
@@ -2,24 +2,22 @@ const rp = require('request-promise');
 const fs = require('fs');
 const sha1 = require('sha1');
 
-const download = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=eb76c4693e64db332bc098e9bc9f6217150ffcf2";
-const send = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=eb76c4693e64db332bc098e9bc9f6217150ffcf2";
+const downloadUrl = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=eb76c4693e64db332bc098e9bc9f6217150ffcf2";
+const sendUrl = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=eb76c4693e64db332bc098e9bc9f6217150ffcf2";
 
-async function getOrigin () {
+async function getDesafio () {
     try {
-      return await rp({ uri: download, json: true });
+      return await rp({ uri: downloadUrl, json: true });
     } catch (err) {
       console.log(err);
     }
 };
 
-function write (answer){
+function write (answer) {
     fs.writeFileSync('answer.json', JSON.stringify(answer));
 }
 
-function decryptMessage (message, numCasas){
-    console.log(message)
-
+function decryptMessage (message, numCasas) {
     var decrypted = "";
 
     var codigo = {
@@ -27,7 +25,7 @@ function decryptMessage (message, numCasas){
         z: 122
     };
 
-    for (let i = 0; i < message.length; i++){
+    for (let i = 0; i < message.length; i++) {
         if(message[i].charCodeAt() >= codigo.a && message[i].charCodeAt() <= codigo.z){
             var charCode = message[i].charCodeAt() - numCasas;
             var newCharCode = charCode;
@@ -42,18 +40,43 @@ function decryptMessage (message, numCasas){
             decrypted += message[i];
         }
     }
-    console.log("decriptado: " + decrypted);
     return decrypted;
 }
 
+function encriptSHA1 (message) {
+    return sha1(message);
+}
 
-async function teste () {
-    answer = await getOrigin();
+async function sendDesafio () {
+    return await rp({
+        uri: sendUrl,
+        method: 'POST',
+        formData: {
+        answer: {
+            value: fs.createReadStream('answer.json'),
+            options: {
+            filename: 'answer.json',
+            contentType: 'multipart/form-data'
+            }
+        }
+        }
+    }).then(function (response){
+        console.log(response);
+    })
+    .catch(function (response){
+        console.log(response);
+    });
+}
+
+async function desafio () {
+    answer = await getDesafio();
     write(answer);
     answer.decifrado = decryptMessage(answer.cifrado.toLowerCase(), answer.numero_casas);
     write(answer);
-    
-    console.log(answer);
+    answer.resumo_criptografico = encriptSHA1(answer.decifrado);
+    write(answer);
+    var response = sendDesafio();
+    console.log(response);
 }
 
-teste();
+desafio();
